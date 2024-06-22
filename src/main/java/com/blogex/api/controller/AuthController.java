e6fc8e10831267232974373315386dd61de53c90
tkdgns0056 <tkdgns0056@gmail.com>
2024-06-22 15:34:26 +0900

    feach  : 인증 코드 추가(session)

--- src/main/java/com/blogex/api/controller/AuthController.java
+++ src/main/java/com/blogex/api/controller/AuthController.java
@@ -1,7 +1,12 @@
 package com.blogex.api.controller;
 
+import com.blogex.api.request.Login;
+import com.blogex.api.response.SessionResponse;
+import com.blogex.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,7 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
 
-
-
+    @PostMapping("/auth/login")
+    public SessionResponse login(@RequestBody Login login){
+        String accessToken = authService.signIn(login);
+        return new SessionResponse(accessToken);
+    }
 }
