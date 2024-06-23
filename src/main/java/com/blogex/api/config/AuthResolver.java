262832667318910397e0a62e3d0429f6007f3f92
tkdgns0056 <tkdgns0056@gmail.com>
2024-06-23 21:02:36 +0900

    feach  : 인증 코드 추가(session)

--- src/main/java/com/blogex/api/config/AuthResolver.java
+++ src/main/java/com/blogex/api/config/AuthResolver.java
@@ -4,12 +4,17 @@ import com.blogex.api.config.data.UserSession;
 import com.blogex.api.domain.Session;
 import com.blogex.api.exception.Unauthorized;
 import com.blogex.api.repositrory.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.net.http.HttpRequest;
 
 
 /**
@@ -17,6 +22,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
  * 그래서 해결해주는 역할로 Argument Resolver를 사용한다.
  */
 @RequiredArgsConstructor
+@Slf4j
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
     private final SessionRepository sessionRepository;
@@ -31,10 +37,21 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization"); // getPamaeter로 가져오면 충돌이 발생할 수 있으므로 header에 담아서 넣는다.
-        if(accessToken == null || accessToken.equals("")){
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class); // getPamaeter로 가져오면 충돌이 발생할 수 있으므로 header에 담아서 넣는다.
+        if(servletRequest == null){
+            log.info("servletRequest null");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+
+        if(cookies.length == 0){
+            log.error("쿠키가 없음");
+            throw new Unauthorized();
+        } // 여기까진 쿠키가 있음.
+
+        String accessToken = cookies[0].getValue();
+
 
         // 데이터베이스 사용자 확인작업
         Session session = sessionRepository.findByAccessToken(accessToken)
--- src/main/java/com/blogex/api/controller/AuthController.java
+++ src/main/java/com/blogex/api/controller/AuthController.java
@@ -5,10 +5,14 @@ import com.blogex.api.response.SessionResponse;
 import com.blogex.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 
 
 @Slf4j
@@ -19,8 +23,23 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login){
+    public ResponseEntity<Object> login(@RequestBody Login login){
         String accessToken = authService.signIn(login);
-        return new SessionResponse(accessToken);
+        // 쿠키 생성 방법
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30)) //쿠키 만료 시간 지정
+                .sameSite("strict")
+                .build();
+        // 쿠기가 어덯게 변한되는지 확인
+        log.info(">>>>>>> cookie={}", cookie.toString());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
+//        return new SessionResponse(accessToken);
     }
 }
